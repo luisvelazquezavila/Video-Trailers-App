0801b989c52d070e20a94ee6efb707c924d3b4e5
luis <luisvelazquezavila@gmail.com>
2024-02-05 00:08:13 -0600

    Cambié el título de la app y ajusté el padding del contenedor de los pósters

--- src/components/movies/MoviesContainer.jsx
+++ src/components/movies/MoviesContainer.jsx
@@ -5,7 +5,7 @@ import Movie from "./Movie";
 export default function MoviesContainer({ movies, selectMovie }) {
 
   return (
-    <Grid container spacing={5} sx={{padding: "2rem", mt: 1}}>
+    <Grid container spacing={5} sx={{padding: {md: "3rem", sm: "1.5rem", xs: "1rem"}}}>
       {movies.map((movie) => (
         <Grid item key={movie.id} xs={12} sm={6} md={4} lg={3}>
           <Movie movie={movie} selectMovie={selectMovie} />
